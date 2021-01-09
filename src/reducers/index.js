ec1185b9618dde5afd4b0989e730db5aad9fd1fc
Hisashin7331 <hiszaszin@gmail.com>
2021-01-09 14:02:32 +0100

    Dark mode

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import appsReducer from 'reducers/appsReducer';
 import themeReducer from 'reducers/themeReducer';
+import notificationsReducer from 'reducers/notificationsReducer';
 
 export default combineReducers({
     appsReducer,
-    themeReducer
+    themeReducer,
+    notificationsReducer
 });
